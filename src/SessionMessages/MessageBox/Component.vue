4e3dd70a2874f7bb301f677eecb454c47c4b365f
ezegyfa2 <darvasvok@gmail.com>
2023-01-29 21:56:50 +0200

    MessageBox

---
+++ src/SessionMessages/MessageBox/Component.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    include Template.pug
+</template>
+
+<script>
+    import SessionMessage from '../SessionMessage.vue'
+
+    export default {
+        mixins: [
+            SessionMessage,
+        ],
+        data() {
+            return {
+                iconClass: ''
+            }
+        }
+    }
+</script>
