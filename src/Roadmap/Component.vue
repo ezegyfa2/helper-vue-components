c3a4e56953761d2c621269370572690620f2a37f
Darvas Istvan <darvasvok@gmail.com>
2023-03-15 12:50:51 +0200

    Roadmap and timeline

---
+++ src/Roadmap/Component.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+    include Template.pug
+</template>
+
+<script>
+    export default {
+        props: {
+            title_section: {
+                type: Object
+            },
+            timeline_section: {
+                type: Object
+            },
+            added_classes: {
+                type: Object,
+                default () {
+                    return {
+                        '.content': 'text-uppercase'
+                    }
+                }
+            }
+        }
+    }
+</script>
---
+++ src/Timeline/Timeline/Component.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+    include Template.pug
+</template>
+
+<script>
+    export default {
+        props: {
+            timeline_item_sections: {
+                type: Array
+            }
+        }
+    }
+</script>
