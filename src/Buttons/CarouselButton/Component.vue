eef260c0e76d9f43e656140b9291aa5dbcba6a80
József Bagota <jozsef.bagota@gmail.com>
2023-07-14 15:51:04 +0200

    carousel button component

---
+++ src/Buttons/CarouselButton/Component.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    include Template.pug
+</template>
+
+<script>
+    export default {
+        props: {
+            dataSlideTitle: {
+                type: String
+            },
+            title: {
+                type: String
+            },
+        }
+    }
+</script>
